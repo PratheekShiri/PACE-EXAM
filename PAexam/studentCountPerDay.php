22e6699f692b3e1602113a8ef5d5c5c7e2c56b58
Nikhil Amin <aminnikhil073@gmail.com>
2019-05-01 21:47:06 +0530

    Faculty Allotments

--- PAexam/studentCountPerDay.php
+++ PAexam/studentCountPerDay.php
@@ -51,6 +51,7 @@
 
     // Truncate table before use
     $truncatestudentCountPerDay = mysqli_query($conn,"TRUNCATE TABLE studentCountPerDay");
+    $truncatefacultyAllotment = mysqli_query($conn,"TRUNCATE TABLE facultyAllotment");
 
     $sql1 = "SELECT * FROM calculateddata";
     $calculatedDataResult = mysqli_query($conn, $sql1);
@@ -79,6 +80,47 @@
         }
         $previousDate = $date;
     }
+
+    //reading available faculties and storing it in array `facultyList`
+    $facultyList = array();
+    $sql00 = "SELECT * FROM facultylist WHERE status = '1'";
+    $facultyListResult = mysqli_query($conn, $sql00);
+    
+    while ($facultyListResultRow = mysqli_fetch_array($facultyListResult)) {
+        array_push($facultyList,$facultyListResultRow['id']);
+    }
+
+
+    $sql01 = "SELECT * FROM studentCountPerDay WHERE studentCount != '0'";
+    $studentCountResult = mysqli_query($conn, $sql01);
+    $currentIndex = 0;
+    $totalFacultyCount = count($facultyList);
+    
+    while ($studentCountResultRow = mysqli_fetch_array($studentCountResult)) {
+        $studentCount = $studentCountResultRow['studentCount'];
+        $date = $studentCountResultRow['date'];
+        $requiredFacultyCount = ceil($studentCount/250);
+        $x = 0;
+        while($x < $requiredFacultyCount){
+
+            if($currentIndex == $totalFacultyCount){
+                $currentIndex=0;
+            }
+            echo('<br> Date: '.$date);
+            echo('<br> Number of Faculty Alloted: '.$requiredFacultyCount);
+            echo('<br> Faculty: '.$facultyList[$currentIndex]);
+
+            $sql = mysqli_query($conn,"INSERT INTO facultyAllotment(`date`,`facultyCount`,`facultyId`)VALUES('$date','$requiredFacultyCount','$facultyList[$currentIndex]')");
+
+            $currentIndex++;
+            $x++;
+        }
+         
+    }
+
+
+
+
 
     $sql2 = "SELECT * FROM studentCountPerDay";
     $studentCountPerDayResult = mysqli_query($conn, $sql2);
@@ -112,6 +154,8 @@
     ';
 
     ?>
+
+
 
 
     <!-- JQuery -->
